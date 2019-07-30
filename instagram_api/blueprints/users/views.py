813cdc716a0519b526be0e2086cfa62f883268f2
flatpackcode2 <colinnoahpeter@gmail.com>
2019-07-30 14:35:34 +0800

    added API endpoint to get list of users

--- instagram_api/blueprints/users/views.py
+++ instagram_api/blueprints/users/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Flask, Blueprint, jsonify, make_response
+from models.user import User
+
 
 users_api_blueprint = Blueprint('users_api',
                              __name__,
@@ -6,4 +8,15 @@ users_api_blueprint = Blueprint('users_api',
 
 @users_api_blueprint.route('/', methods=['GET'])
 def index():
-    return "USERS API"
+    users = User.select()
+
+    user_list = []
+    for user in users:
+        user_list.append({
+           'id': user.id,
+           'first_name':user.first_name,
+           'last_name' : user.last_name,
+           'profile_pic_url':user.profile_image_url
+        })
+    response= {'data':user_list}
+    return make_response(jsonify(response),200)
